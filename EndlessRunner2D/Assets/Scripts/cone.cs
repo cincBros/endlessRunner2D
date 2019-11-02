a9225d6b32db8917ce8579ecb7c1ebb9c7c178de
santivg99 <santi.antonipous@gmail.com>
2019-11-02 13:00:37 +0100

    Cones and Bird

--- EndlessRunner2D/Assets/Scripts/cone.cs
+++ EndlessRunner2D/Assets/Scripts/cone.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class cone : spawnable
 {
+    public Sprite[] coneSprites = new Sprite[3];
+    private SpriteRenderer spr;
+
     public override void initialize()
     {
         name = "cone";
-        yPos = 4f;
+        yPos = 4.2f;
+        spr = GetComponent<SpriteRenderer>();
+        int rand = Random.Range(0, coneSprites.Length);
+        spr.sprite = coneSprites[rand];
     }
 }
