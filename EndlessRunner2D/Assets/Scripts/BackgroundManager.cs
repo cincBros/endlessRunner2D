8f799226eed898fc181da622783926e21f61a030
Pantomaca <57219633+Pantomaca@users.noreply.github.com>
2019-11-23 14:48:52 +0100

    Background loop

---
+++ EndlessRunner2D/Assets/Scripts/BackgroundManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundManager : MonoBehaviour
+{
+    [SerializeField]
+    private BackgroundElement[] backgroundElements;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (true) 
+        { 
+            foreach (BackgroundElement element in backgroundElements)
+            {
+                element.Move();
+            }
+        }
+    }
+}
